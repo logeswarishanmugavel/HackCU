8a3e95ca2f5168e398c40efe2bbea2c1557777a7
santro92 <jayramsan@gmail.com>
2017-04-22 13:29:37 -0600

    flask boiler plate code

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+# Grabs the folder where the script runs.
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+
+DEBUG = True
+
+# Secret key for session management. You can generate random strings here:
+
+# http://clsc.net/tools-old/random-string-generator.php
+
+SECRET_KEY = 'my precious'
+
+# Connect to the database
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/hackcu'
---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, Date, Boolean
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
+
+engine = create_engine('mysql+pymysql://root:root@localhost:3306/hackcu', echo=False)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+# Models
+
+
+# Create tables.
+Base.metadata.create_all(bind=engine)
