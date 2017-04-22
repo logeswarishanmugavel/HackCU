edb1c7277f281045db4296b76029b982a182cdfe
athithyaa <atpa2251@colorado.edu>
2017-04-22 17:24:25 -0600

    adding facebook sdk setup

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,27 @@
 'use strict';
 
-angular.module('bikeApp', [
- 'ngRoute'
- ]).
-config(['$routeProvider',
-     function($routeProvider) {
+var bikeApp = angular.module('bikeApp', [
+    'facebook', 'ngRoute'
+ ]);
+
+bikeApp.config(['$routeProvider','FacebookProvider',
+     function($routeProvider,FacebookProvider) {
          $routeProvider.
              when('/', {
-                 templateUrl: '/static/pages/welcome.html',
+                 templateUrl: '/static/pages/welcome.html'
              }).
              otherwise({
                  redirectTo: '/'
              });
+         FacebookProvider.init('405452559812718')
     }]);
+
+bikeApp.controller('loginController',[
+    '$scope','Facebook', function ($scope,Facebook) {
+        $scope.login = function () {
+            Facebook.login(function (response) {
+                console.log(response);
+            });
+        }
+    }
+]);
