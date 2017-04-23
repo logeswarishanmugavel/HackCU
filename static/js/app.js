846b838657df7a5ee27bdfd3a24009d6eaa59873
athithyaa <atpa2251@colorado.edu>
2017-04-22 18:35:08 -0600

    adding facebook sdk setup changes - adding fb api 3.0

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,44 @@
 'use strict';
 
 var bikeApp = angular.module('bikeApp', [
-    'facebook', 'ngRoute'
+    'ngRoute'
  ]);
 
-bikeApp.config(['$routeProvider','FacebookProvider',
-     function($routeProvider,FacebookProvider) {
+bikeApp.run(function($rootScope, facebook) {
+    $rootScope.Facebook = facebook;
+});
+
+
+bikeApp.factory('facebook', ['$window', function($window) {
+
+    //get FB from the global (window) variable.
+    var FB = $window.FB;
+
+    // gripe if it's not there.
+    if(!FB) throw new Error('Facebook not loaded');
+
+    //make sure FB is initialized.
+    FB.init({
+        appId : '405452559812718',
+        xfbml: true,
+        version: 'v3.0'
+    });
+
+    return {
+        // a me function
+        login : function(callback) {
+            FB.login(callback,{scope:'email,public_profile'});
+        },
+        
+        logout: function (callback) {
+            FB.logout(callback);
+        }
+
+    }
+}]);
+
+bikeApp.config(['$routeProvider',
+     function($routeProvider) {
          $routeProvider.
              when('/', {
                  templateUrl: '/static/pages/welcome.html'
@@ -13,14 +46,18 @@ bikeApp.config(['$routeProvider','FacebookProvider',
              otherwise({
                  redirectTo: '/'
              });
-         FacebookProvider.init('405452559812718')
     }]);
 
 bikeApp.controller('loginController',[
-    '$scope','Facebook', function ($scope,Facebook) {
+    '$scope','facebook', function ($scope,facebook) {
         $scope.login = function () {
-            Facebook.login(function (response) {
-                console.log(response);
+            facebook.login(function (data) {
+                console.log(data);
+            });
+        };
+        $scope.logout = function () {
+            facebook.login(function (data) {
+                console.log(data);
             });
         }
     }
